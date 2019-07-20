ea46cdfaf4166d6bfd2ee57f0c106b0a857899d7
David Inoa <davidinoa@gmail.com>
2019-07-20 14:02:23 -0700

    create collection page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import { selectCollection } from '../../redux/shop/shop.selectors';
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => {
+  return (
+    <div className="category">
+      <h2>CATEGORY PAGE</h2>
+      <div>
+        {collection.items.map((item) => (
+          <CollectionItem />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
