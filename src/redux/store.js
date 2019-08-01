2aab73c13eb4c6d68d77ea8d6e26c7625b366037
David Inoa <davidinoa@gmail.com>
2019-08-01 12:33:34 -0700

    create root saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
 import createSagaMiddleware from 'redux-saga';
-import { fetchCollectionsStart } from './shop/shop.sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
@@ -14,6 +14,6 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
