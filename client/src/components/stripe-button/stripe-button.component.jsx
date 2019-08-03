64c66ab27955ee102894c4b46cd23b014679db60
David Inoa <davidinoa@gmail.com>
2019-08-02 22:06:01 -0700

    send payment request from client

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_25eC5BJ4mPDjUFEUnnxg1u8G007P9zKid7';
 
-  const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then(response => {
+        alert('Payment successful');
+      })
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert('There was an issue with your payment. Please use the provided card information');
+      });
   };
 
   return (
