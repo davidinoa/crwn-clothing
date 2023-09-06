5e99eaedb06ac3e2f89acae06b719f89c71da6fe
David Inoa <davidinoa@gmail.com>
2023-09-06 00:23:01 -0400

    update packages and fixes for vercel deployment

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const config = {
-  apiKey: 'AIzaSyCovoqql83jfMtu7nZhNyguVrw00a11cFg',
-  authDomain: 'crwn-db-dil.firebaseapp.com',
-  databaseURL: 'https://crwn-db-dil.firebaseio.com',
-  projectId: 'crwn-db-dil',
-  storageBucket: '',
-  messagingSenderId: '626793475190',
-  appId: '1:626793475190:web:fc02445f700e92f8',
+  apiKey: "AIzaSyCovoqql83jfMtu7nZhNyguVrw00a11cFg",
+  authDomain: "crwn-db-dil.firebaseapp.com",
+  databaseURL: "https://crwn-db-dil.firebaseio.com",
+  projectId: "crwn-db-dil",
+  storageBucket: "",
+  messagingSenderId: "626793475190",
+  appId: "1:626793475190:web:fc02445f700e92f8",
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
@@ -30,7 +30,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -43,7 +43,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
